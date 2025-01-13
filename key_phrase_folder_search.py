3cce5545c9ef5cdcce839caa204d043347a2080e
Viper287 <max@digikata.com>
2025-01-12 18:43:43 -0700

    Made important clarifications in the description and comments of the search_folders function

--- key_phrase_folder_search.py
+++ key_phrase_folder_search.py
@@ -3,8 +3,8 @@ import os
 
 def search_folders(directory, search_term):
     """
-    Function to search directories for files/directories with keyword or phrases in their file names.
-    Only works if only one item has the key-phrase.
+    Function to search directories for folders with keyword or phrases in their names.
+    Only returns the first folder found with the search-term in the name.
     :param directory: path to the directory to be searched
     :param search_term: key-phrase string
     :return: if a file is found it will be returned, else None will be returned
@@ -13,10 +13,10 @@ def search_folders(directory, search_term):
     if not search_term:
         return None
 
-    # Walks through the directory and checks if the provided search term is present in the name of each file
+    # Walks through the directory and checks if the provided search term is present in the name of each folder
     for root, dirs, files in os.walk(directory):
         for dir_name in dirs:
             if search_term in dir_name:
-                # If the search term is found it's immediately returned
+                # If the search term is found in the folder name it's immediately returned
                 result = os.path.join(root, dir_name)
                 return result
